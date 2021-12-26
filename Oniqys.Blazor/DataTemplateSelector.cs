069f1f680861ba99e46fb38b5ab238b097544a84
山本 礼貴 <nenaaki@gmail.com>
2021-12-27 07:01:27 +0900

    add DataTemplateSelector

---
+++ Oniqys.Blazor/DataTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oniqys.Blazor
+{
+    public class DataTemplateSelector
+    {
+        private readonly Dictionary<Type, ComponentBase> _selector;
+
+        public DataTemplateSelector(IReadOnlyDictionary<Type, ComponentBase> selector)
+        {
+            _selector = new Dictionary<Type, ComponentBase>(selector);
+        }
+
+        public ComponentBase GetComponent(Type dataType)
+        {
+            return _selector.TryGetValue(dataType, out ComponentBase component) ? component : null;
+        }
+    }
+}
